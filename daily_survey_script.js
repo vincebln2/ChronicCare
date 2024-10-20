99e5ceb04a7437640479ee010c44b6b53eb47f09
LeftNutTM <vinceb6371@gmail.com>
2024-10-20 12:57:35 -0500

    Stable Version - firebase initialization in firebase_config.js now

--- daily_survey_script.js
+++ daily_survey_script.js
@@ -1,18 +1,4 @@
-let firebaseConfig = {
-    apiKey: "AIzaSyAbMZqwYT6jLlK9gHcqq9IHpuN5R8JrSIA",
-    authDomain: "chroniccare-bb97e.firebaseapp.com",
-    projectId: "chroniccare-bb97e",
-    databaseURL: "https://chroniccare-bb97e-default-rtdb.firebaseio.com/",
-    storageBucket: "chroniccare-bb97e.appspot.com",
-    messagingSenderId: "936032624692",
-    appId: "1:936032624692:web:7aabe39990bdf428ab6a61",
-    measurementId: "G-0TEBG69FJQ"
-
-};
-firebase.initializeApp(firebaseConfig);
-
-let messagesRef = firebase.database()
-    .ref('Collected Data');
+//firebase config now in firebase_config.js
 
 document.getElementById('form')
     .addEventListener('submit', submitForm);
@@ -44,17 +30,4 @@ function getRadioVal(name) {
         }
     }
     return null; // return null if no radio button is selected
-}
-
-// save message to firebase
-function saveMessage(name, email, fatigue, appetite, comment) {
-    let newMessageRef = messagesRef.push();
-    newMessageRef.set({
-        name: name,
-        email: email,
-        fatigue: fatigue,
-        appetite: appetite,
-        comment: comment
-
-    });
 }
