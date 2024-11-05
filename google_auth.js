b6b85ed1cb1a12cca0b258e1cf3ec860a9f4ccdc
Vince <97649067+vincebln2@users.noreply.github.com>
2024-11-05 09:59:50 -0600

    Update google_auth.js

--- google_auth.js
+++ google_auth.js
@@ -2,15 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebas
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 
 // firebase configuration
-let firebaseConfig = {
-    apiKey: "AIzaSyAbMZqwYT6jLlK9gHcqq9IHpuN5R8JrSIA",
-    authDomain: "chroniccare-bb97e.firebaseapp.com",
-    projectId: "chroniccare-bb97e",
-    databaseURL: "https://chroniccare-bb97e-default-rtdb.firebaseio.com/",
-    storageBucket: "chroniccare-bb97e.appspot.com",
-    messagingSenderId: "936032624692",
-    appId: "1:936032624692:web:7aabe39990bdf428ab6a61",
-    measurementId: "G-0TEBG69FJQ"
+let firebaseConfig = {//config key would go here
 };
 
 // initialize Firebase
