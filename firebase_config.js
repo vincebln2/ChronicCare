9ef565d7a61ae85acf3a719c851004a440c5d7e2
Vince <97649067+vincebln2@users.noreply.github.com>
2024-11-05 09:59:21 -0600

    Update firebase_config.js
    got rid of api key

--- firebase_config.js
+++ firebase_config.js
@@ -1,13 +1,5 @@
 // Import the necessary Firebase modules
-let firebaseConfig = {
-    apiKey: "AIzaSyAbMZqwYT6jLlK9gHcqq9IHpuN5R8JrSIA",
-    authDomain: "chroniccare-bb97e.firebaseapp.com",
-    projectId: "chroniccare-bb97e",
-    databaseURL: "https://chroniccare-bb97e-default-rtdb.firebaseio.com/",
-    storageBucket: "chroniccare-bb97e.appspot.com",
-    messagingSenderId: "936032624692",
-    appId: "1:936032624692:web:7aabe39990bdf428ab6a61",
-    measurementId: "G-0TEBG69FJQ"
+let firebaseConfig = {//config would go here
 
 };
 firebase.initializeApp(firebaseConfig);
